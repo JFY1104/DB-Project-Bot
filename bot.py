cfa23452440aed47773fbd19aa2b6bd2eb5ddbcf
JFY1104 <jack31304@gmail.com>
2023-05-08 00:33:09 +0800

    123

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+from typing import Optional
 import discord
 from discord.ext import commands, tasks
 from itertools import cycle
 import os
 import asyncio
+
 
 bot = commands.Bot(command_prefix='*', intents=discord.Intents.all())
 
@@ -14,6 +16,7 @@ bot_status = cycle(["Status 1", "Status 2", "Status 3"])
 async def change_status():
     await bot.change_presence(activity=discord.Game(next(bot_status)))
 
+
 @bot.event
 async def on_ready():
     print("Bot is Online!")
@@ -21,15 +24,41 @@ async def on_ready():
         await change_status()
         await asyncio.sleep(10)
 
+
 async def load():
     for filename in os.listdir("DB-Project-Bot\cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
+    
+
 
 @bot.command()
 async def reload(ctx, extension):
     await bot.reload_extension(f'cogs.{extension}')
     await ctx.send(f'RELOAD cogs.{extension} DONE')
+
+
+class Myselect(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label="red", value="1", description="this is red"),
+            discord.SelectOption(label="green", value="2",description="this is green"),
+            discord.SelectOption(label="blue", value="3",description="this is blue")
+        ]
+        super().__init__(placeholder='Choose your favourite character',
+                         min_values=1, max_values=1, options=options)
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f'Your favourite colour is {self.values[0]}')
+class menuview(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(Myselect())
+
+@bot.command()
+async def menu(ctx):
+    view = menuview()
+    await ctx.send("hi", view=view)
+
 
 async def main():
     async with bot:
--- cogs/test.py
+++ cogs/test.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+
+
 class test(commands.Cog):
     def __init__(self,bot) -> None:
         self.bot = bot 
@@ -13,6 +15,9 @@ class test(commands.Cog):
     async def ping(self,ctx):
         bot_lantency = round(self.bot.latency * 1000)
         await ctx.send(f"ping is {bot_lantency}ms.")
+
+    
+
 
 async def setup(bot):
     await bot.add_cog(test(bot))
