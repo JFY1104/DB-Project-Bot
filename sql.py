3e6f60d954c07529ec6c6dc6d48c6f5d59352b92
JFY1104 <jack31304@gmail.com>
2023-05-27 12:46:31 +0800

    Update sql.py

--- sql.py
+++ sql.py
@@ -9,25 +9,32 @@ try:
         database="testdb",
         charset="utf8"
         )
+    selectformula = "select * from 光錐資料表"
+    deleteformula = "delete from 光錐資料表 where 光錐名稱 = '於夜色中'"
+
     mycursor = mydb.cursor()
-    sqlformula = "insert into 角色 (名字,稀有度,命途) values (%s,%s,%s)"
-    character = [("三月七","四星","存護"),
-                 ("丹恆","四星","巡獵"),
-                 ("停雲","四星","同偕"),
-                 ("娜塔莎","四星","豐饒"),
-                 ("虎克","四星","毀滅")]
-    mycursor.execute("select * from 角色")
+    selectformula = "select * from 光錐資料表"
+    sqlformula = "insert into 光錐資料表 (光錐名稱,故事,命途,攻擊力,生命力,防禦力,效果) \
+                  values (%s,%s,%s,%s,%s,%s,%s)"
+    new_data = [("於夜色中","省略","巡獵","123","456","789","省略"),
+                ("拂曉之前","省略","智識","123","114","514","省略"),
+                ("論劍","省略","巡獵","987","654","321","省略")]
+    mycursor.executemany(sqlformula,new_data)
+    mydb.commit()
+    mycursor.execute(selectformula)
     myresult = mycursor.fetchall()
     for row in myresult:
         print(row)
-        
-    mydb.commit()
+    
+
 except mysql.connector.Error as e:
     print("error")
     print(e)
 
 
-
-
-
+"""     selectformula = "select * from 光錐資料表"
+    deleteformula = "delete from 光錐資料表 where 光錐名稱 = '於夜色中'" 
+        deleteformula = "delete from 光錐資料表 where 光錐名稱 = '於夜色中'" 
+         mycursor.execute("delete from 角色 where 名字 = '虎克'")  
+        mycursor.execute("select * from 角色")  """
 
